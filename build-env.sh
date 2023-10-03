93eb97ba6885b10db69b208c22728629b00e71fe
Kevin Stratford <kevin@epcc.ed.ac.uk>
2023-10-03 11:56:59 +0100

    Add JMMP configuration arch files

--- build-env.sh
+++ build-env.sh
@@ -11,6 +11,7 @@ module load cray-netcdf-hdf5parallel
 # "archer2-gnu-r8-d8"  adds "-fdefault-real-8 -fdefault-double-8" (*)
 # "archer2-cce"        Cray CCE default precision
 # "archer2-cce-r8"     Cray CCE adds "-sreal64"
-# (*) the only operational choice at the moment
+# "archer2-gnu-jmmp"   Config from JMMP (*)
+# (*) the only operational choices at the moment
 
-export build_env_arch_id="archer2-gnu-r8-d8"
+export build_env_arch_id="archer2-gnu-jmmp"
